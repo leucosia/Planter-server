bd26080567dc397365f3dfcc7b79eabd5acc69ed
ykm989 <ykm989@naver.com>
2025-02-07 15:26:49 +0900

    [feat] Swagger Setting
    프론트엔드와 협업을 위한 Swagger 문서 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Planter API Swagger")
+    .setDescription("Planter API 문서")
+    .setVersion("1.0.0")
+    .addTag("oauth", "로그인 관련입니다.")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
