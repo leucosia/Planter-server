342f5bb99759b72b6649f61558586c25e44ae95c
ykm989 <ykm989@naver.com>
2025-02-07 16:15:08 +0900

    [del] oauth 삭제
    oauth 삭제

--- src/oauth/dto/update-oauth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOauthDto } from './create-oauth.dto';
-
-export class UpdateOauthDto extends PartialType(CreateOauthDto) {}
--- src/oauth/oauth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OauthService } from './oauth.service';
-import { OauthController } from './oauth.controller';
-
-@Module({
-  controllers: [OauthController],
-  providers: [OauthService],
-})
-export class OauthModule {}
