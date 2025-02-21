17422fbc8465fe12fe0bc966d477ac55ec2fed75
ykm989 <ykm989@naver.com>
2025-02-21 16:05:58 +0900

    [feat] 변수 명 수정
    - UseGuard 상위에 적용
    - user_id -> userId 변수 명 변경

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -9,6 +9,7 @@ import { UpdateTodoResponseDto } from './dto/update.todo.response.dto';
 
 
 @Controller('todos')
+@UseGuards(AuthGuard('jwt'))
 @ApiBearerAuth()
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
@@ -19,7 +20,7 @@ export class TodosController {
   })
   @ApiBody({ type: CreateTodoBodyDto })
   @ApiResponse({ type: CreateTodoResponseDTO})
-  @UseGuards(AuthGuard('jwt'))
+  
   @Post()
   async create(@Body() createTodoDto: CreateTodoBodyDto, @Request() req) {
     const userId = req.user.userId
@@ -30,7 +31,6 @@ export class TodosController {
     summary: "TODO 목록 조회",
     description: "TODO 목록 조회 API\n현재 유저의 TODO 목록을 가져옵니다."
   })
-  @UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll(@Request() req) {
     const userId = req.user.userId
@@ -41,34 +41,31 @@ export class TodosController {
     summary: "TODO 개별 조회",
     description: "TODO ID 입력 시 TODO를 조회"
   })
-  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) todo_id: number, @Request() req) {
-    const user_id = req.user.userId
-    return await this.todosService.findOne(todo_id, user_id)
+    const userId = req.user.userId
+    return await this.todosService.findOne(todo_id, userId)
   }
 
   @ApiOperation({
     summary: "TODO 업데이트",
     description: "TODO 업데이트 API"
   })
-  @UseGuards(AuthGuard("jwt"))
   @ApiBody({ type: UpdateTodoBodyDto })
   @ApiResponse({ type: UpdateTodoResponseDto })
   @Post(':id')
   async update(@Param('id', ParseIntPipe) todo_id: number, @Request() req, @Body() updateTodoDto: UpdateTodoBodyDto) {
-    const user_id = req.user.user_id
-    return await this.todosService.update(todo_id, user_id, updateTodoDto)
+    const userId = req.user.userId
+    return await this.todosService.update(todo_id, userId, updateTodoDto)
   }
 
   @ApiOperation({
     summary: "TODO 삭제",
     description: "TODO 삭제 API"
   })
-  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) todo_id: number, @Request() req) {
-    const user_id = req.user.userId
-    return await this.todosService.remove(todo_id, user_id)
+    const userId = req.user.userId
+    return await this.todosService.remove(todo_id, userId)
   }
 }
