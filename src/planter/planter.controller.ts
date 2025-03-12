955c9f5717b4a21701daab050637d2a69f506a6c
ykm989 <ykm989@naver.com>
2025-03-12 15:02:19 +0900

    [feat] 유저 식물 정보 조회 API

--- src/planter/planter.controller.ts
+++ src/planter/planter.controller.ts
@@ -16,6 +16,16 @@ export class PlanterController {
   @Get()
   async getUserPlantFromToken(@Request() req) {
     const userId = req.user.userId;
-    return this.planterService.getUserPlant(userId);
+    return this.planterService.getUserPlantFromUserId(userId);
+  }
+
+  @ApiOperation({
+    summary: "유저 식물 정보 API (식물 ID)",
+    description: "식물 ID값을 바탕으로 유저 식물 정보를 반환합니다."
+  })
+  @Get(':id')
+  async getUserPlantFromId(@Param('id', ParseIntPipe) id: number, @Request() req) {
+    const userId = req.user.userId;
+    return this.planterService.getUserPlantFromPlantId(id, userId);
   }
 }
--- src/planter/planter.service.ts
+++ src/planter/planter.service.ts
@@ -7,11 +7,20 @@ export class PlanterService {
     private prisma: PrismaService
   ){}
 
-  async getUserPlant(userId: number) {
+  async getUserPlantFromUserId(userId: number) {
     return await this.prisma.user_plants.findFirst({
       where: {
         user_id: userId,
         plants_is_done: false
+      }
+    })
+  }
+
+  async getUserPlantFromPlantId(userPlantId: number, userId: number) {
+    return await this.prisma.user_plants.findUnique({
+      where: {
+        user_plant_id: userPlantId,
+        user_id: userId
       }
     })
   }
