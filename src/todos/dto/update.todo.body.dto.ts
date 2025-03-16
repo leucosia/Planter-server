ba41c40e0d0685feb4acf8d4acb397e3976b974f
ykm989 <ykm989@naver.com>
2025-03-17 02:09:27 +0900

    [fix] #36 TODO 수정 실패 버그 수정
    - TODO 수정 실패 원인은 데이터 타입은 데이터 타입을 string이 아니라 date로 받아서 생기는 문제 이를 string으로 수정하고 IsDateString으로 제한 걸어서 해결
    - TODO 수정 시 completed todo도 맞게 수정

--- src/todos/dto/update.todo.body.dto.ts
+++ src/todos/dto/update.todo.body.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString } from "class-validator";
+import { IsDateString, IsNumber, IsString } from "class-validator";
 
 export class UpdateTodoBodyDto {
     @ApiProperty({
@@ -22,10 +22,12 @@ export class UpdateTodoBodyDto {
     @ApiProperty({
         description: "TODO 시작일, 수정 없을 시 기존 내용 작성"
     })
-    start_date: Date;
+    @IsDateString()
+    start_date: string;
     
     @ApiProperty({
         description: "TODO 마감일, 수정 없을 시 기존 내용 작성"
     })
-    end_date: Date;
+    @IsDateString()
+    end_date: string;
 }
