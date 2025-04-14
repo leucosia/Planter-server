d919491efcfd25e0a557e461d238c9834755c432
김경호 <ykm989@naver.com>
2025-04-14 18:09:34 +0900

    [chore] planter API 수정 및 추가
    - Get /planter/all-plants 추가
    - Get /planter/{id} 추가
    - /planter -> /planter/all-user-plants 수정
    - /planter/{id} -> /planter/user-plant/{id} 수정

--- src/planter/planter.service.ts
+++ src/planter/planter.service.ts
@@ -7,42 +7,56 @@ export class PlanterService {
     private prisma: PrismaService
   ){}
 
-  async getUserPlantFromUserId(userId: number) {
-    try {
-      return await this.prisma.user_plants.findMany({
-        where: {
-          user_id: userId
-        },
-        include: {
-          plant: true
-        }
-      });
-    } catch(error) {
-      console.log(error);
-      throw new UnauthorizedException('INVALID_REQUEST');
-    }
-  }
-
-  async getUserPlantFromPlantId(userPlantId: number, userId: number) {
+  // User ID를 기반으로 User 식물 정보를 얻는 함수
+  async getUserPlant(userId: number, userPlantId: number) {
     try {
       return await this.prisma.user_plants.findUnique({
         where: {
-          user_plant_id: userPlantId,
-          user_id: userId
+          user_id: userId,
+          user_plant_id: userPlantId
+        }
+      })
+    } catch(error) {
+      console.log(error);
+      throw new BadRequestException('INVALID_REQUEST');
+    }
+  }
+
+  // Plant ID를 기반으로 식물 정보를 얻는 함수
+  async getPlant(plantId: number) {
+    try {
+      return await this.prisma.plants.findUnique({
+        where: {
+          plant_id: plantId
         }
       });
     } catch(error) {
       console.log(error);
-      throw new UnauthorizedException('INVALID_REQUEST');
+      throw new BadRequestException('INVALID_REQUEST');
     }
   }
 
-  async getAllPlanter() {
+  // 모든 식물 정보를 가져오는 함수
+  async getAllPlants() {
     try {
       return await this.prisma.plants.findMany();
     } catch (error) {
       console.log(error);
       throw new BadRequestException("NO_DATA_FOUND")
+    }
+  }
+
+  // 모든 유저 식물 정보를 가져오는 함수
+  async getAllUserPlants(userId: number) {
+    try {
+      return await this.prisma.user_plants.findMany({
+        where: {
+          user_id: userId
+        }
+      });
+    } catch(error) {
+      console.log(error);
+      throw new BadRequestException('INVALID_REQUEST');
     }
   }
 }
